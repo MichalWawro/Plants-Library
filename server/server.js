32bc10a642fe08d754db2b7a9476fc5ad3ba689c
unknown <mwawro77@gmail.com>
2023-06-21 09:20:18 +0200

    pull request

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ import {router as apiRouter} from './routes/api.js';
 const app = express();
 const port = 5000;
 
-mongoose.connect("mongodb+srv://c00kier:fXRTiEvwcsOUXvaD@cluster0.c98jvc7.mongodb.net/");
+mongoose.connect("mongodb+srv://1657358923:UnEN9kYdfFEVX1Xi@165735892.x9rcfss.mongodb.net/");
 
 app.use(express.json());
 app.use(cors());
