61a42bad051a288a796639d7bb99cef5d9ab62dc
Pawel Ignaczak <ignak265@gmail.com>
2023-06-22 09:31:22 +0200

    Set backgrounf for whole page

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,12 +3,13 @@ import bcrypt from 'bcrypt';
 
 //models
 import { User } from "../models/User.js";
+import { Profile } from "../models/Profile.js";
 
 const router = express.Router();
 
 router.route("/myplants")
     .get((req, res) => {
-        res.status(200).send("Działa");
+        res.status(200).json("Działa");
     })
 
 router.route("/users")
@@ -16,14 +17,19 @@ router.route("/users")
         try {
             const salt = await bcrypt.genSalt();
             const hashedPassword = await bcrypt.hash(req.body.password, salt);
-            await User.create({
+            const user = await User.create({
                 userName: req.body.username,
                 password: hashedPassword,
                 email: req.body.email
             })
-            res.status(201).send("Created");
+            await Profile.create({
+                userId: user._id,
+                plants: [],
+                achivements: []
+            })
+            res.status(201).json("Created");
         } catch (error) {
-            res.status(400).send("Something went wrong... Try Again");
+            res.status(400).json("Something went wrong... Try Again");
         }
     })
 
@@ -33,16 +39,16 @@ router.route("/users/login")
             const users = await User.find({});
             const user = users.find(user => user.userName === req.body.username);
             if (user === undefined) {
-                return res.status(400).send("Cannot find user");
+                return res.status(400).json("Cannot find user");
             }
             if (await bcrypt.compare(req.body.password, user.password)) {
                 res.redirect(301, "http://localhost:3000/home");
             } else {
-                return res.status(400).send("Not Allowed")
+                return res.status(400).json("Not Allowed")
             }
 
         } catch (error) {
-            res.status(400).send("Something went wrong... Try Again");
+            res.status(400).json("Something went wrong... Try Again");
         }
     })
 
