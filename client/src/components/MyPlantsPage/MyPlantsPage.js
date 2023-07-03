d30f9a5fe22db2c6b50f5429d2bf6abc96f3dfc8
Pawel Ignaczak <ignak265@gmail.com>
2023-07-03 15:00:26 +0200

    watering system - bar creation

--- client/src/components/MyPlantsPage/MyPlantsPage.js
+++ client/src/components/MyPlantsPage/MyPlantsPage.js
@@ -1,7 +1,12 @@
 import { useEffect } from "react";
-import "./MyPlantsPage.css"
+import "./MyPlantsPage.css";
+
+import wateringCan from "../../assets/watering_can.png";
+import yellowStar from "../../assets/yellow_star.png";
+import grayStar from "../../assets/gray_star.png";
 
 function MyPlantsPage({ profileDetails, setProfileDetails }) {
+    
 
     useEffect(() => {
         fetch("http://localhost:5000/api/profile", {
@@ -9,10 +14,15 @@ function MyPlantsPage({ profileDetails, setProfileDetails }) {
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ userId: profileDetails[0].userId })
         })
-        .then(response => response.json())
-        .then(data => setProfileDetails(data))
-        .catch(error => console.error(error));
+            .then(response => response.json())
+            .then(data => setProfileDetails(data))
+            .catch(error => console.error(error));
     }, [])
+
+
+    const handleRemoveFromFav = () => {
+
+    }
 
     return (
         <>
@@ -22,6 +32,11 @@ function MyPlantsPage({ profileDetails, setProfileDetails }) {
                     <div className="flex-column-center-center" key={"fav" + index}>
                         <img src={element.default_image.thumbnail} alt="plant" />
                         <h2>{element.common_name}</h2>
+                        <div className="manage-bar flex-row-center-center">
+                            <img className="manage-icons watering-icon" src={wateringCan} alt="watering can"/>
+                            <img className="manage-icons star-on" src={yellowStar} alt="favourite" onClick={handleRemoveFromFav}/>
+                            <img className="manage-icons star-off" src={grayStar} alt="removed favourite" onClick={handleRemoveFromFav}/>
+                        </div>
                     </div>
                 )}
             </div>
